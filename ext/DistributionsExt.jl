cb9ed7626aff1bfc70830dfd3bfca7c721d0cdab
itsdfish <itsdfish@gmail.com>
2023-07-13 11:19:27 -0400

    add example

---
+++ ext/DistributionsExt.jl
@@ -0,0 +1,7 @@
+isdefined(Base, :get_extension) ? (using Distributions) : (using ..Distributions)
+
+using PackageExtensionsExample
+
+function cool_function()
+    # a function that depends on Distributions
+end
