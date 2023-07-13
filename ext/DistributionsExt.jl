4381862d13bf58143f60d947e347a7ce6abddc88
itsdfish <itsdfish@gmail.com>
2023-07-13 13:43:35 -0400

    export cool_function

--- ext/DistributionsExt.jl
+++ ext/DistributionsExt.jl
@@ -1,10 +1,15 @@
 module DistributionsExt
 
     using PackageExtensionsExample
+    import PackageExtensionsExample: cool_function
     import Distributions: logpdf
-    
+
     function logpdf(d::MyType)
         println("calling logpdf(d::MyType)")
+    end
+
+    function cool_function()
+        println("calling cool_function()")
     end
 
 end
--- src/PackageExtensionsExample.jl
+++ src/PackageExtensionsExample.jl
@@ -1,7 +1,10 @@
 module PackageExtensionsExample
 
     export MyType 
+    export cool_function
 
     struct MyType end
+
+    function cool_function end
 
 end
